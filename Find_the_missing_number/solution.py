1108e439f151d3be4b9aa47b5c9bf224f7937817
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-12-07 16:31:43 +0530

    Added another solution for the problem

--- Find_the_missing_number/solution.py
+++ Find_the_missing_number/solution.py
@@ -8,3 +8,12 @@ for i in range(len(ls)):
     if ls[i] != (i + 1):
         print(i + 1)
         break
+
+# --------------------------------
+#         Solution 2nd
+# --------------------------------
+ls = [1, 2, 4, 6, 3, 7, 8]
+
+n = len(ls + 1)      # As there are n-1 elements but we need to calculate summation for n elements
+summation = n*(n+1) // 2
+print(summation - sum(ls))
