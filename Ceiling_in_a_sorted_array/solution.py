73034c18501bdeac14a4f5a077105583b4928c57
Abhay_Dhiman <abhaysdhimans@gmail.com>
2020-12-24 17:06:00 +0530

    Solution for second half

--- Ceiling_in_a_sorted_array/solution.py
+++ Ceiling_in_a_sorted_array/solution.py
@@ -21,7 +21,31 @@ def ceiling(arr, start, end, x):
         return ceiling(arr, start, mid - 1, x)
 
 
+# floar
+def floaring(arr, start, end, x):
+    if x >= arr[end]:
+        return arr[end]
+    if x < arr[start]:
+        return -1
+    
+    mid = (start + end) // 2
+    
+    if x == arr[mid]:
+        return arr[mid]
+
+    elif x > arr[mid]:
+        if arr[mid + 1] > x and (mid + 1) <= end:
+            return arr[mid]
+        return floaring(arr, mid + 1, end, x)
+
+    else:
+        if arr[mid - 1] <= x and (mid - 1) >= start:
+            return arr[mid - 1]
+        return floaring(arr, start, mid - 1, x)
+
+
 ls = [1, 2, 8, 10, 10, 12, 19]
-x = 12.1
+x = 9
 end = len(ls) - 1
-print(ceiling(ls, 0, end, x))
+print("ceil value --> ", ceiling(ls, 0, end, x))
+print("floar value --> ", floaring(ls, 0, end, x))
