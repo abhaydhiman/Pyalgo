4806b4d4e5f582caf3c1e3257e77d06cc8e50f7f
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-01-03 12:17:13 +0530

    Solution number second

--- Maximum_Length_Bitonic_Subarray/solution.py
+++ Maximum_Length_Bitonic_Subarray/solution.py
@@ -29,32 +29,34 @@ def my_func(arr):
             max_num = inc_ls[i] + dec_ls[i] - 1
     
     return inc_ls, dec_ls, max_num
-    
-    
-    # length = len(arr)
-    # start = 0
-    # end = length - 1
-    # ls = []
-    
-    # while start < end:
-    #     if arr[start] > arr[start + 1]:
-    #         start += 1
-        
-    #     elif arr[end] > arr[end - 1]:
-    #         end -= 1
-        
-    #     print(arr[start])
-    #     print(arr[end])
-    #     if arr[start] < arr[start+1] and arr[end] < arr[end - 1]:
-    #         ls.append(arr[start])
-    #         ls.append(arr[end])
-    #         start += 1
-    #         end -= 1
-    
-    # return ls
 
 
 ls = [12, 4, 78, 90, 45, 23, 12]
 # ls1 = [20, 4, 1, 2, 3, 4, 2, 10]
 print(my_func(ls))
 # print(my_func(ls1))
+
+def my_func_2(arr):
+    length = len(arr)
+    counter = 1
+    count = 0
+    start = 0
+    nextstart = 0
+    
+    while count < (length - 1):
+        
+        while (count < length - 1) and (arr[count] <= arr[count + 1]):
+            count += 1
+        
+        while (count < length - 1) and (arr[count] >= arr[count + 1]):
+            if (count < length - 1) and (arr[count] > arr[count + 1]):
+                nextstart = count + 1
+            count += 1
+        
+        counter = max(counter, count - (start - 1))
+        start = nextstart
+    
+    return counter
+
+
+print(my_func_2(ls))
