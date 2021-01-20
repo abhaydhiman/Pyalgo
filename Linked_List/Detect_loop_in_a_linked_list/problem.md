236e5ea3a29a1fce2ebaa43b92d9f8c2ddecbb70
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-01-20 12:46:13 +0530

    Loop checking in the linked list

---
+++ Linked_List/Detect_loop_in_a_linked_list/problem.md
@@ -0,0 +1,13 @@
+# Detect loop in a linked list
+
+## Given a linked list, check if the linked list has loop or not. Below diagram shows a linked list with a loop
+
+```txt
+1 ---> 2 ---> 3
+       ^      |
+       |      |
+       |      V
+       5 <--- 4
+```
+
+https://www.geeksforgeeks.org/detect-loop-in-a-linked-list/
---
+++ Linked_List/Detect_loop_in_a_linked_list/solution.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+        self.traversed = False
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = self.rear = None
+
+def check_loop(node):
+    temp = node.head
+    
+    if temp == None:
+        print("Empty!!")
+        return
+    
+    while temp:
+        if temp.traversed:
+            print("Loop Exist!!")
+            return
+        
+        temp.traversed = True
+        temp = temp.next
+    
+    print("Loop Does Not Exist!!")
+
+
+linked_list = LinkedList()
+first = Node(1)
+second = Node(2)
+third = Node(3)
+forth = Node(4)
+fifth = Node(5)
+
+linked_list.head = linked_list.rear = first
+first.next = second
+second.next = third
+third.next = forth
+forth.next = fifth
+fifth.next = second
+
+check_loop(linked_list)
