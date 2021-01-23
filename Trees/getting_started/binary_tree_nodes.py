4d722a098ff5cf1db998ac8ee789de05eb57ef61
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-01-23 17:24:26 +0530

    Intro to trees

---
+++ Trees/getting_started/binary_tree_nodes.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
